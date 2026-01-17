2799f8f4e003fa5bc85f9c3aed68d5f0e8b35187
strlghf <strlghf@gmail.com>
2026-01-17 18:27:13 -0300

    Adding async cases

---
+++ 5.fs-async-await.js
@@ -0,0 +1,13 @@
+const { readFile } = require("node:fs/promises");
+
+(async () => {
+  console.log("Leyendo el primer archivo...")
+  const text = await readFile("./archivo.txt", "utf-8")
+  console.log(text)
+
+  console.log("--- Hacer cosas mientras lee el archivo...")
+
+  console.log("Leyendo el segundo archivo...")
+  const secondText = await readFile("./archivo2.txt", "utf-8")
+  console.log(secondText)
+}) ()
