c35e1efaa25babb4ec37d091b0f723a9edc25ec1
strlghf <strlghf@gmail.com>
2026-01-19 09:02:37 -0300

    path, ls, process

---
+++ 6.ls.js
@@ -0,0 +1,21 @@
+const fs = require("node:fs/promises")
+
+// fs.readdir(".", (err, files) => { // without fs/promises
+//   if (err) {
+//     console.error("Error al leer el directorio: ", err)
+//     return;
+//   }
+
+//   files.forEach(file => {
+//     console.log(file)
+//   })
+// })
+
+fs.readdir(".") // with fs/promises
+  .then(files => {
+    files.forEach(file => console.log(file))
+  })
+  .catch(err => {
+    console.error(`Error al leer el directorio ${err}`)
+    return
+  })
---
+++ 7.process.js
@@ -0,0 +1,11 @@
+// console.log(process.argv)
+
+// process.exit(1)
+
+// process.on("exit", () => {
+
+// })
+
+console.log(process.cwd())
+
+console.log(process.env.PEPITO);
