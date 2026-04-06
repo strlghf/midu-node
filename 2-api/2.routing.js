aa709c1a8b9183acce405ac510ac32a63be25beb
strlghf <strlghf@gmail.com>
2026-04-06 13:28:51 -0400

    2ch

--- 2-api/2.routing.js
+++ 2-api/2.routing.js
@@ -1,47 +1,47 @@
-const http = require('node:http')
-const dittoJSON = require('./pokemon/ditto.json')
+const http = require('node:http');
+const dittoJSON = require('./pokemon/ditto.json');
 
 function processRequest (req, res) {
-  const { method, url } = req
+  const { method, url } = req;
 
   switch (method) {
     case 'GET':
       switch (url) {
         case '/pokemon/ditto':
-          res.setHeader('Content-Type', 'application/json; charset=utf-8')
-          return res.end(JSON.stringify(dittoJSON))
+          res.setHeader('Content-Type', 'application/json; charset=utf-8');
+          return res.end(JSON.stringify(dittoJSON));
         default:
-          res.statusCode = 404
-          res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-          return res.end('Not Found 404')
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+          return res.end('Not Found 404');
       }
     case 'POST':
       switch (url) {
         case '/pokemon': {
-          let body = ''
+          let body = '';
 
           req.on('data', chunk => {
-            body += chunk.toString()
+            body += chunk.toString();
           })
-
+J
           req.on('end', () => {
-            const data = JSON.parse(body)
-            res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' })
-            res.end(JSON.stringify(data))
+            const data = JSON.parse(body);
+            res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' });
+            res.end(JSON.stringify(data));
           })
 
-          break
+          break;
         }
         default:
-          res.statusCode = 404
-          res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-          return res.end('Not Found 404')
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+          return res.end('Not Found 404');
       }
   }
 }
 
-const server = http.createServer(processRequest)
+const server = http.createServer(processRequest);
 
 server.listen(3000, () => {
-  console.log('server listening on port http://localhost:3000')
+  console.log('server listening on port http://localhost:3000');
 })
--- 2-api/3.express.js
+++ 2-api/3.express.js
@@ -1,13 +1,13 @@
-const ditto = require('./pokemon/ditto.json')
-const express = require('express')
-const app = express()
+const ditto = require('./pokemon/ditto.json');
+const express = require('express');
+const app = express();
 
-app.disable('x-powered-by')
+app.disable('x-powered-by');
 
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.PORT ?? 3000;
 
 // express.json() hace lo mismo que el codigo de abajo
-app.use(express.json())
+app.use(express.json()); // <= MIDDLEWARE
 
 // app.use((req, res, next) => { // .use se executa siempre
 //   if (req.method !== 'POST') return next()
@@ -27,19 +27,23 @@ app.use(express.json())
 //   })
 // })
 
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+})
+
 app.get('/pokemon/ditto', (req, res) => {
-  res.json(ditto)
+  res.json(ditto);
 })
 
 app.post('/pokemon', (req, res) => {
   // req.body guardar en base de datos
-  res.status(201).json(req.body)
+  res.status(201).json(req.body);
 })
 
 app.use((req, res) => {
-  res.status(404).send('Not Found 404')
+  res.status(404).send('Not Found 404');
 })
 
 app.listen(PORT, () => {
-  console.log(`server listening on port http://localhost:${PORT}`)
+  console.log(`server listening on port http://localhost:${PORT}`);
 })
