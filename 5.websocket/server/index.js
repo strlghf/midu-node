c79d2e3a67a1da6d95c3ec3daf8212a2843a4a64
strlghf <strlghf@gmail.com>
2026-04-13 17:24:16 -0400

    .

--- 5.websocket/server/index.js
+++ 5.websocket/server/index.js
@@ -4,13 +4,25 @@ import { Server } from "socket.io"
 import { createServer } from "node:http"
 
 const port = process.env.PORT ?? 3000;
+
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("an user has connected");
+
+  socket.on("disconnect", () => {
+    console.log("an user has disconnected");
+  })
+})
+
 app.use(logger("dev"));
 
 app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/client/index.html");
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 })
